6803bdcbb940e217b22274a7ebf8e83cdb0de83b
law <liliangwei90@gmail.com>
2025-06-07 16:37:26 +0800

    routing: route parameter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 
 <nav>
     <a href="/">Home</a>
+    <a href="/blog">Blog</a>
     <a href="/about">About</a>
 </nav>
 
---
+++ src/routes/blog/[num]/+page.svelte
@@ -0,0 +1,6 @@
+<!-- To create routes with dynamic parameters, use square brackets around a valid variable name. 
+ For example, a file like src/routes/blog/[slug]/+page.svelte will create a route that matches 
+ /blog/one, /blog/two, /blog/three and so on.
+ -->
+<h1>blog post</h1>
+
