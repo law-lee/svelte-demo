cff9f43d2ac8d2271c0c8b71945cde33ab557f49
law <liliangwei90@gmail.com>
2025-06-07 17:05:36 +0800

    loading data: page data

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,14 @@
 <!-- To create routes with dynamic parameters, use square brackets around a valid variable name. 
  For example, a file like src/routes/blog/[slug]/+page.svelte will create a route that matches 
  /blog/one, /blog/two, /blog/three and so on.
- -->
+
 <h1>blog post</h1>
 
+-->
+
+<script>
+    let { data } = $props();
+</script>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
