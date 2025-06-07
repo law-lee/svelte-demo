2669c9222af1c1e4a8cf7b94ca8d048e9f466128
law <liliangwei90@gmail.com>
2025-06-07 21:18:53 +0800

    headers and cookies: setting cookies

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,9 +4,19 @@ which we’ll learn about later) you have access to a setHeaders function,
 which — unsurprisingly — can be used to set headers on the response.
 
 export function load({ setHeaders }) {
-	setHeaders({
-		'Content-Type': 'text/html'
-	});
+    setHeaders({
+        'Content-Type': 'text/html'
+    });
 }
 
 */
+
+export function load({ cookies }) {
+    const visited = cookies.get('visited');
+
+    cookies.set('visited', 'true', { path: '/' });
+
+    return {
+        visited: visited === 'true'
+    };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	let { data } = $props();
+</script>
+
+<h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
 <h1>home</h1>
 <p>this is the home page.</p>
