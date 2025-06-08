a9d9126413559723f14ecdbfdca05dd9aeb2ccee
law <liliangwei90@gmail.com>
2025-06-08 13:40:03 +0800

    shared modules: the  alias

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	let { data } = $props();
+    import { message } from '$lib/message.js';
 </script>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
 <h1>home</h1>
 <p>this is the home page.</p>
+
+<p>{message}</p>
---
+++ src/routes/a/deeply/nested/route/+page.svelte
@@ -0,0 +1,11 @@
+<!--
+Sometimes, code is used in multiple places. When this happens, it’s useful to have a place to put them that can be accessed 
+by all routes without needing to prefix imports with ../../../../. In SvelteKit, that place is the src/lib directory. 
+Anything inside this directory can be accessed by any module in src via the $lib alias.
+-->
+<script>
+	import { message } from '$lib/message.js';
+</script>
+
+<h1>a deeply nested route</h1>
+<p>{message}</p>
