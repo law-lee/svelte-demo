80d9e847d7bef1e7c44ca3f42301b3a842142bf6
law <liliangwei90@gmail.com>
2025-06-10 22:36:16 +0800

    app state: page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script>
+    import { page } from '$app/state';
     let { children } = $props();
 </script>
 
 <nav>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/a/deeply/nested/route">a deeply nested route</a>
-    <a href="/todos">Todo Lists</a>
-    <a href="/roll">Roll Dice</a>
-    <a href="/todov2">Todo v2</a>
+    <a href="/" aria-current={page.url.pathname === '/'}>Home</a>
+    <a href="/blog" aria-current={page.url.pathname === '/blog'}>Blog</a>
+    <a href="/about" aria-current={page.url.pathname === '/about'}>About</a>
+    <a href="/a/deeply/nested/route" aria-current={page.url.pathname === '/a/deeply/nested/route'}>a deeply nested route</a>
+    <a href="/todos" aria-current={page.url.pathname === '/todos'}>Todo Lists</a>
+    <a href="/roll" aria-current={page.url.pathname === '/roll'}>Roll Dice</a>
+    <a href="/todov2" aria-current={page.url.pathname === '/todov2'}>Todo v2</a>
 </nav>
 
 <!--  The {@render children()} tag is where the page content will be rendered -->
