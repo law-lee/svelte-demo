b661bc3b5f2ea6ddb9b5c58fc54c7ecd7f9f1e90
law <liliangwei90@gmail.com>
2025-06-10 22:36:49 +0800

    app state: page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,15 @@
+<!--
+SvelteKit makes three readonly state objects available via the $app/state module — page, navigating and updated. 
+The one you’ll use most often is page, which provides information about the current page:
+
+url — the URL of the current page
+params — the current page’s parameters
+route — an object with an id property representing the current route
+status — the HTTP status code of the current page
+error — the error object of the current page, if any (you’ll learn more about error handling in later exercises)
+data — the data for the current page, combining the return values of all load functions
+form — the data returned from a form action
+-->
 <script>
     import { page } from '$app/state';
     let { children } = $props();
