2eeaaeebe92c89a904f300151fd800fc157a3833
law <liliangwei90@gmail.com>
2025-06-10 22:53:44 +0800

    app state: navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,19 +11,32 @@ data — the data for the current page, combining the return values of all load 
 form — the data returned from a form action
 -->
 <script>
-    import { page } from '$app/state';
-    let { children } = $props();
+	import { page, navigating } from '$app/state';
+	let { children } = $props();
 </script>
 
 <nav>
-    <a href="/" aria-current={page.url.pathname === '/'}>Home</a>
-    <a href="/blog" aria-current={page.url.pathname === '/blog'}>Blog</a>
-    <a href="/about" aria-current={page.url.pathname === '/about'}>About</a>
-    <a href="/a/deeply/nested/route" aria-current={page.url.pathname === '/a/deeply/nested/route'}>a deeply nested route</a>
-    <a href="/todos" aria-current={page.url.pathname === '/todos'}>Todo Lists</a>
-    <a href="/roll" aria-current={page.url.pathname === '/roll'}>Roll Dice</a>
-    <a href="/todov2" aria-current={page.url.pathname === '/todov2'}>Todo v2</a>
+	<a href="/" aria-current={page.url.pathname === '/'}>Home</a>
+	<a href="/blog" aria-current={page.url.pathname === '/blog'}>Blog</a>
+	<a href="/about" aria-current={page.url.pathname === '/about'}>About</a>
+	<a href="/a/deeply/nested/route" aria-current={page.url.pathname === '/a/deeply/nested/route'}
+		>a deeply nested route</a
+	>
+	<a href="/todos" aria-current={page.url.pathname === '/todos'}>Todo Lists</a>
+	<a href="/roll" aria-current={page.url.pathname === '/roll'}>Roll Dice</a>
+	<a href="/todov2" aria-current={page.url.pathname === '/todov2'}>Todo v2</a>
 </nav>
 
+<!--
+The navigating object represents the current navigation. 
+When a navigation starts — because of a link click, or a back/forward navigation, or a programmatic goto — 
+the value of navigating will become an object with the following properties:
+
+from and to — objects with params, route and url properties
+type — the type of navigation, e.g. link, popstate or goto
+-->
+{#if navigating.to}
+	navigating to {navigating.to.url.pathname}
+{/if}
 <!--  The {@render children()} tag is where the page content will be rendered -->
 {@render children()}
